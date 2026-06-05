cab3f4f2cdaa452194e2806ed18ce9ac0677f671
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2026-06-05 18:40:20 -0300

    Feat: Exibir teores faltantes

--- src/interfaces/DiverseContentRange.ts
+++ src/interfaces/DiverseContentRange.ts
@@ -8,8 +8,9 @@ export interface DiverseContentRangeResponseDto {
 // Lista de Sufixos (usada para tipagem e iteração)
 export type NutrientSuffix = 
     | "carbono_organico" | "materia_organica" | "calcio" | "magnesio" 
-    | "saturacao_aluminio" | "saturacao_bases" | "boro" | "cobre" 
-    | "ferro" | "manganes" | "zinco";
+    | "aluminio" | "aluminio_mais_hidrogenio" | "ctc_efetiva" | "ctc_ph7"
+    | "saturacao_aluminio" | "saturacao_bases" | "ph_agua" | "ph_cacl2"
+    | "boro" | "cobre" | "ferro" | "manganes" | "zinco";
 
 // DTO para Criação (POST) - Chaves em Português
 export interface DiverseContentRangeCreateRequestDto {
