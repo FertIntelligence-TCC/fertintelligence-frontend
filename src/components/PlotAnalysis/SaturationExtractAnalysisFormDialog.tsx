c7c63e12c5e0d23a3bbb1c10dc7f199f62f1e0f8
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2026-06-11 16:03:57 -0300

    Fix: Ajustar Dureza e RAS no Extrato de Saturação no frontend

--- src/components/PlotAnalysis/SaturationExtractAnalysisFormDialog.tsx
+++ src/components/PlotAnalysis/SaturationExtractAnalysisFormDialog.tsx
@@ -500,9 +500,9 @@ export const SaturationExtractAnalysisFormDialog = ({
 
                                             <SectionHeader title="Dureza e Indicadores" colorPalette="pink" />
                                             <Grid templateColumns="repeat(4, 1fr)" gap={4}>
-                                                <Field label="Dureza CaCO3 (mg/L)" type="number" value={ext.durezaCaCO3} onChange={e => handleChangeExtract(ext.tempId, 'durezaCaCO3', parseFloat(e.target.value))} readOnly={isReadOnly} />
-                                                <Field label="Dureza Total (mg/L)" type="number" value={ext.durezaTotalCaCO3} onChange={e => handleChangeExtract(ext.tempId, 'durezaTotalCaCO3', parseFloat(e.target.value))} readOnly={isReadOnly} />
-                                                <Field label="RAS" type="number" value={ext.ras} onChange={e => handleChangeExtract(ext.tempId, 'ras', parseFloat(e.target.value))} readOnly={isReadOnly} />
+                                                <Field label="Dureza CaCO3 (mg/L de CaCO3)" type="number" value={ext.durezaCaCO3} onChange={e => handleChangeExtract(ext.tempId, 'durezaCaCO3', parseFloat(e.target.value))} readOnly={isReadOnly} />
+                                                <Field label="Dureza Total (mg/L de CaCO3)" type="number" value={ext.durezaTotalCaCO3} onChange={e => handleChangeExtract(ext.tempId, 'durezaTotalCaCO3', parseFloat(e.target.value))} readOnly={isReadOnly} />
+                                                <Field label="RAS [(cmolc/L)^0,5]" type="number" value={ext.ras} onChange={e => handleChangeExtract(ext.tempId, 'ras', parseFloat(e.target.value))} readOnly={isReadOnly} />
                                                 <Field label="PST (%)" type="number" value={ext.pst} onChange={e => handleChangeExtract(ext.tempId, 'pst', parseFloat(e.target.value))} readOnly={isReadOnly} />
                                             </Grid>
                                         </Box>
